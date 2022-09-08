d29db7f16897944fdf043325503eaffa7d365903
mochacr0 <1951120093@sv.ut.edu.vn>
2022-09-08 10:00:50 +0700

    update: add new permission to user: delete asset

--- application/src/main/java/org/thingsboard/server/service/security/permission/CustomerUserPermissions.java
+++ application/src/main/java/org/thingsboard/server/service/security/permission/CustomerUserPermissions.java
@@ -61,7 +61,7 @@ public class CustomerUserPermissions extends AbstractPermissions {
     private static final PermissionChecker customerEntityPermissionChecker =
             new PermissionChecker.GenericPermissionChecker(Operation.READ, Operation.READ_CREDENTIALS,
                     Operation.READ_ATTRIBUTES, Operation.READ_TELEMETRY, Operation.RPC_CALL, Operation.CLAIM_DEVICES,
-                    Operation.WRITE, Operation.WRITE_ATTRIBUTES, Operation.WRITE_TELEMETRY) {
+                    Operation.WRITE, Operation.WRITE_ATTRIBUTES, Operation.WRITE_TELEMETRY, Operation.DELETE) {
 
                 @Override
                 @SuppressWarnings("unchecked")
